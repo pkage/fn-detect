f6e4b4f5b96f0f0ab65f995068cc8f7148135d58
Patrick Kage <quadnix1@gmail.com>
2018-03-10 15:39:32 +0000

    Add SQLite provider into flask requests

--- fnd/__init__.py
+++ fnd/__init__.py
@@ -1,4 +1,13 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+
+DATABASE = '../scraper/fn-detect.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 def create_app(debug=True):
     app = Flask(__name__)
@@ -7,5 +16,11 @@ def create_app(debug=True):
     # blueprints!
     from fnd.main import main_blueprint
     app.register_blueprint(main_blueprint)
+
+    @app.teardown_appcontext
+    def close_db_conn(exception):
+        db = getattr(g, '_database', None)
+        if db is not None:
+            db.close()
 
     return app
